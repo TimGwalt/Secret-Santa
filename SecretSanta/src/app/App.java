b50d5db88255e715bff8bbdaa3a9b1c8d7822d35
Tim Gwalt <gwaltneyt15@students.ecu.edu>
2019-11-12 16:35:25 -0500

    Added picker function. Added an assignment printer function.

--- SecretSanta/src/app/App.java
+++ SecretSanta/src/app/App.java
@@ -1,10 +1,22 @@
 package app;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
 
     public static Scanner kbd = new Scanner(System.in);
+
+
+    /**
+     * PrintArray() prints the given array of person objects.
+     * @param people
+     */
+    private static void printArray(Person[] people) {
+        for(int i = 0; i < people.length; i++) {
+            System.out.println(people[i].getName() + " -> " + people[i].getAssignment());
+        }
+    }
 
     /**
      * AddPerson()gathers person data and returns 
@@ -16,13 +28,31 @@ public class App {
         boolean couple;
         
         System.out.println("Name: ");
-        name = kbd.nextLine();
+        name = kbd.next();
         System.out.println("Is this person part of a couple? (true/false)");
         couple = kbd.nextBoolean();
         System.out.println("Partner's name: ");
-        partner = kbd.nextLine();
+        partner = kbd.next();
             
         return new Person(name, couple, partner);
+    }
+
+    
+    private static void picker(Person[] people) {
+        for(int i = 0; i < people.length; i++) {
+            int rand = new Random().nextInt(people.length);
+
+            // This loop makes sure that no one is assigned multiple times.
+            while(rand == i || people[i].isAssigned()) {
+                rand = new Random().nextInt(people.length);
+            }
+
+            people[i].setAssignment(people[rand].getName());
+            people[i].setAssign(true);
+        }
+
+        printArray(people);
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -39,7 +69,8 @@ public class App {
         for (int i = 0; i < count; i++) {
             people[i] = AddPerson();
         }
-        
+
+        picker(people);
 
         // Testing the person objects
         // Remove later
